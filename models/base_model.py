e519488363f1b6faf2aaafd49bd4547816aaa658
Nazarena <nazarenaaranda15@gmail.com>
2022-10-14 18:35:59 -0700

    add T in isoformat

--- models/base_model.py
+++ models/base_model.py
@@ -7,20 +7,21 @@ import models
 
 
 class BaseModel:
-    """ This will be define all common attributes/methods 
+    """ This will be define all common attributes/methods
     for other classes """
 
     def __init__(self, *args, **kwargs):
+        var = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             for key, value in kwargs.items():
                 if key == "id":
-                    self.id = value 
+                    self.id = value
                 elif key == __class__:
                     pass
                 elif key == "created_at":
-                    self.created_at = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.created_at = datetime.strptime(value, var)
                 elif key == "updated_at":
-                    self.updated_at = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.updated_at = datetime.strptime(value, var)
                 else:
                     setattr(self, key, value)
         else:
@@ -32,11 +33,11 @@ class BaseModel:
         """ This will be return a dictionary that contains
         key and values in __dic__ of the instance """
         dicnew = self.__dict__.copy()
-        dicnew["created_at"] = self.created_at.isoformat()
-        dicnew["updated_at"] = self.updated_at.isoformat()
+        dicnew["created_at"] = self.created_at.isoformat("T")
+        dicnew["updated_at"] = self.updated_at.isoformat("T")
         dicnew["__class__"] = self.__class__.__name__
         return dicnew
-    
+
     def save(self):
         """ this is useful when you update and has the current time """
         self.udated_at = datetime.now()
